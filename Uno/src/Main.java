5b7668e3532656c056f86f0e81b4e109dcaae10d
kitzoyan <110635359+kitzoyan@users.noreply.github.com>
2024-01-08 12:52:05 -0500

    Declared all classes. Official start of programming.
    Made all classes needed. All cards have been moved into a folder.

--- Uno/src/Main.java
+++ Uno/src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Robin");
-        System.out.println("Yo");
-        System.out.println("test");
-        System.out.println("Yo Robin");
-        System.out.println("Exclusive to branch");
-        // Comment!
+
     }
 }
