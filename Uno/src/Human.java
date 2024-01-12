1a97ef4421b98b56cdc1453a4331898e64a1d16f
thegodofnoob <88257959+thegodofnoob@users.noreply.github.com>
2024-01-12 03:06:50 -0500

    Revert "Revert "Modified the cpu to not play according to the difficulty, started on the human class""
    This reverts commit dae13a3c049b741eb6a07f2e4cefb66e0916caf5.

--- Uno/src/Human.java
+++ Uno/src/Human.java
@@ -1,3 +1,12 @@
-public class Human {
+public class Human extends Player {
+    private boolean tutorialMode = false;
 
+    public Human(String name) {
+        super(name);
+    }
+
+    public Human(String name, Deck deck, boolean tutorialMode) {
+        super(deck, name);
+        this.tutorialMode = tutorialMode;
+    }
 }
