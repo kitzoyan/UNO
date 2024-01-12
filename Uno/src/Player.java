12dbb5680a931b46a6940b2d1fe05021ad670e0d
agslaj <88257959+thegodofnoob@users.noreply.github.com>
2024-01-12 03:00:03 -0500

    Modified the cpu to not play according to the difficulty, started on the human class

--- Uno/src/Player.java
+++ Uno/src/Player.java
@@ -1,3 +1,30 @@
-public class Player {
+import Cards.*;
 
+public abstract class Player {
+    protected String name;
+    protected Deck deck;
+
+    /**
+     * Class Constructor
+     * This class create a new player with the name and new deck
+     * 
+     * @param name a String representing the name of the player
+     */
+    public Player(String name) {
+        this.name = name;
+        deck = new Deck();
+    }
+
+    /**
+     * Class Constructor
+     * This class load a player with the given name and new deck
+     * 
+     * @param name a String representing the name of the player
+     */
+    public Player(Deck deck, String name) {
+        this.name = name;
+        this.deck = deck;
+    }
+
+    public abstract Card play(Card currentCard);
 }
